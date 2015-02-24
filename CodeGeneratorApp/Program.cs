fc33a9ada1cc49916d5729ca8f1cce02e09ddd2d
Sanjay Yadav <yadav-sanjay@live.com>
2015-02-24 13:37:50 -0700

    commit to second branch

--- CodeGeneratorApp/Program.cs
+++ CodeGeneratorApp/Program.cs
@@ -10,8 +10,18 @@ namespace CodeGeneratorApp
     {
         static void Main(string[] args)
         {
+            Add();
             Console.WriteLine("master branch");
             Console.WriteLine("added in  github console");
+            Console.ReadLine();
+        }
+
+        static void Add()
+        {            
+            string num1 = Console.ReadLine();
+            string num2 = Console.ReadLine();
+            int sum = int.Parse(num1) + int.Parse(num2);
+            Console.WriteLine(sum);
             Console.ReadLine();
         }
     }
